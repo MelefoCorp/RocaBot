2aa2b5be3c02b448f58addf6dbf8f2e53b58ac80
Melefo <melefo@melefo.xyz>
2021-07-30 03:18:37 +0200

    Adding base for Commands Modules

---
+++ Roca.Bot/Modules/Chat/Self.cs
@@ -0,0 +1,18 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using Roca.Mongo;
+using System.Threading.Tasks;
+
+namespace Roca.Bot.Modules.Chat
+{
+    public class Self : BaseCommandModule
+    {
+        [Command("ping")]
+        public async Task Ping(CommandContext ctx)
+        {
+            var account = await ctx.User.GetAccount(false).ConfigureAwait(false);
+            await ctx.RespondAsync("Pong!").ConfigureAwait(false);
+            await account.Save().ConfigureAwait(false);
+        }
+    }
+}
--- Roca.Bot/RocaBot.cs
+++ Roca.Bot/RocaBot.cs
@@ -1,5 +1,8 @@
 ﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Roca.Bot
@@ -7,12 +10,14 @@ namespace Roca.Bot
     public class RocaBot
     {
         private DiscordShardedClient _client;
+        private CommandsNextConfiguration _cmdConfig;
+        private IReadOnlyDictionary<int, CommandsNextExtension>? _cmds;
 
-        public RocaBot(string token)
+        public RocaBot(IConfiguration configuration)
         {
             _client = new DiscordShardedClient(new DiscordConfiguration
             {
-                Token = token,
+                Token = configuration["RocaBot:Token"],
                 TokenType = TokenType.Bot,
                 AlwaysCacheMembers = true,
                 UseRelativeRatelimit = true,
@@ -24,10 +29,34 @@ namespace Roca.Bot
                 MinimumLogLevel = LogLevel.Warning
 #endif
             });
+
+            _cmdConfig = new CommandsNextConfiguration
+            {
+                CaseSensitive = false,
+                EnableDefaultHelp = false,
+                EnableDms = false,
+                EnableMentionPrefix = false,
+                IgnoreExtraArguments = true,
+                StringPrefixes = new string[] { configuration["RocaBot:DefaultPrefix"] },
+            };
+
+            _cmds = null;
         }
 
-        public async Task Start() => await _client.StartAsync().ConfigureAwait(false);
+        public async Task Start()
+        {
+            _cmds = await _client.UseCommandsNextAsync(_cmdConfig).ConfigureAwait(false);
 
-        public async Task Stop() => await _client.StopAsync().ConfigureAwait(false);
+            foreach (var cmd in _cmds)
+                cmd.Value.RegisterCommands(GetType().Assembly);
+
+            await _client.StartAsync().ConfigureAwait(false);
+        }
+
+        public async Task Stop()
+        {
+            await _client.StopAsync().ConfigureAwait(false);
+            _cmds = null;
+        }
     }
 }
